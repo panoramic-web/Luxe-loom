6f7b637c80cf4397592304c6ca7b71d7d65d34db
Panoramic web <92629860+panoramic-web@users.noreply.github.com>
2024-02-06 08:59:23 +0530

    Update js

---
+++ assets/js/product_list.js
@@ -0,0 +1,100 @@
+/* Specifies the product category name Insert the price in dollors without the $ sign */
+
+let products = [
+   t_shirts = [
+      {
+         category_name: 'T-shirts'
+      }, {
+         product_code: 1001,
+         product_image: './assets/items/t shirts/TFETTERS-Fashion-Men-T-shirt-Slim-Fit-Custom-T-shirt-Crease-Design-Long-Stylish-Luxury-V__83553.1550172485.jpg',
+         product_name: 'Long stylish luxury v',
+         product_description: 'Available in black, white, grey',
+         product_price: '1500',
+         product_added_date: '2023-10-29'
+   }, {
+         product_code: 1002,
+         product_image: './assets/items/t shirts/2018-Fashion-Men-T-shirt-Slim-Fit-Custom-T-shirt-Crease-Design-Long-Stylish-Luxury__74411.1523708594.jpg',
+         product_name: 'Long stylish luxury',
+         product_description: 'Available in black, white, grey',
+         product_price: '1750',
+         product_added_date: '2023-10-06'
+   }, {
+         product_code: 1003,
+         product_image: './assets/items/t shirts/2018-Fashion-Men-T-shirt-Slim-Fit-Custom-T-shirt-Crease-Design-Long-Stylish-Luxury__33850.1523708593.jpg',
+         product_name: 'Long stylish luxury',
+         product_description: 'Available in black, white, grey',
+         product_price: '1550',
+         product_added_date: '2023-10-15'
+   }, {
+         product_code: 1004,
+         product_image: './assets/items/t shirts/2018-Fashion-Mens-Casual-T-Shirts-Slim-Fit-Short-Sleeve-T-Shirt-Men-s-shirts-Tee.jpg_Q90.jpg_.jpg',
+         product_name: 'Short sleeve t-shirt',
+         product_description: 'Available in black, white, grey',
+         product_price: '1200',
+         product_added_date: '2023-09-15'
+   }, {
+         product_code: 1005,
+         product_image: './assets/items/t shirts/2020-Autumn-Brand-Clothing-100-Cotton-Men-Clothes-T-Shirt-Long-Sleeve-T-Shirts-Mens-Casual.jpg',
+         product_name: 'Long sleeve t-shirt',
+         product_description: 'Available in black, white, grey',
+         product_price: '950',
+         product_added_date: '2023-09-29'
+   }, {
+         product_code: 1006,
+         product_image: './assets/items/t shirts/6-Designs-Mens-T-Shirt-Slim-Fit-Crew-Neck-T-shirt-Men-Short-Sleeve-Shirt-Casual__30249.1537167076.jpg',
+         product_name: 'Short sleeve crew neck',
+         product_description: 'Available in black, white, grey',
+         product_price: '1600',
+         product_added_date: '2023-09-20'
+
+   }, {
+         product_code: 1007,
+         product_image: './assets/items/t shirts/Charcoal Grat T.jpg',
+         product_name: 'Long sleeve v',
+         product_description: 'Available in black, white, grey',
+         product_price: '1450',
+         product_added_date: '2023-09-07'
+   }, {
+         product_code: 1008,
+         product_image: './assets/items/t shirts/New-Design-T-Shirt-Short-Sleeve-Casual-Mens-Polo-Shirt.jpg',
+         product_name: 'Short sleeve polo t-shirt',
+         product_description: 'Available in black, white, grey',
+         product_price: '1500',
+         product_added_date: '2023-09-30'
+   }, {
+         product_code: 1009,
+         product_image: './assets/items/t shirts/full-sleeve-woolen-t-shirt-500x500.jpg',
+         product_name: 'Short sleeve polo t-shirt',
+         product_description: 'Available in black, white, grey',
+         product_price: '1400',
+         product_added_date: '2023-10-10'
+   }, {
+         product_code: 1010,
+         product_image: './assets/items/t shirts/men-27s-designer-t-shirt-500x500.jpg',
+         product_name: 'Short sleeve polo t-shirt',
+         product_description: 'Available in black, white, grey',
+         product_price: '1500',
+         product_added_date: '2023-10-17'
+   }
+   ],
+
+jeans = [
+      {
+         category_name: 'Jeans'
+   }, {
+         product_code: 1011,
+         product_image: './assets/items/jeans/15886958_1.jpeg',
+         product_name: 'Slim fit dark brown jeans',
+         product_description: 'Available in black, white, grey',
+         product_price: '2500',
+         product_added_date: '2023-10-25'
+   }, {
+         product_code: 1012,
+         product_image: './assets/items/jeans/How-Your-Jeans-Should-Fit-Ashley-Weston-Mens-Clothing-Fit-Guide.jpg',
+         product_name: 'Slim fit jeans',
+         product_description: 'Available in black, white, grey',
+         product_price: '3000',
+         product_added_date: '2023-10-22'
+   }
+]
+];
